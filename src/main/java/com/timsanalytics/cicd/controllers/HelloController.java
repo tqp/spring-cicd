de310a98c5883001d4efde359801bf24c64ce9b6
tpowers <tpowers@ridgelineintl.com>
2021-04-22 22:13:44 -0400

    First Commit

---
+++ src/main/java/com/timsanalytics/cicd/controllers/HelloController.java
@@ -0,0 +1,25 @@
+package com.timsanalytics.cicd.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        String message = "Hello AWS Continuous Delivery!";
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            message += " From host: " + ip;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+
+}
+
